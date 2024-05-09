29aaa22e924d1ccf1c49bc6c037d89fad8257ab2
Chintan-G1995 <153381573+Chintan-G1995@users.noreply.github.com>
2024-05-10 00:42:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './layout.scss'
+import Navbar from './components/Navbar/Navbar'
+import HomePage from './routes/HomePage/HomePage'
+
+function App(){
+  return (
+    <div className='layout'>
+     <div className='navbar'>
+     <Navbar/>
+     </div>
+    
+    <div className='content'>
+      <HomePage/>
+    </div>
+    </div>
+    
+
+  )
+}
+
+export default App
---
+++ src/routes/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HomePage.scss'
+
+function HomePage () {
+  return (
+    <div className='homePage'>
+        <div className="textContainer">Text</div>
+        <div className="imageContainer">
+            <img src="/bg.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
