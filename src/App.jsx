fdcc621e441208bb470fc2c48286491f924c9a19
Chintan-G1995 <153381573+Chintan-G1995@users.noreply.github.com>
2024-05-14 14:28:34 +0530

    My-PC
    Second-Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
-import './layout.scss'
-import Navbar from './components/Navbar/Navbar'
-import HomePage from './routes/HomePage/HomePage'
+import React, { Children } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./layout.scss";
+import ListPage from "./routes/listPage/ListPage";
+import Layout from "./routes/layout/Layout";
+import HomePage from "./routes/HomePage/HomePage";
+import SinglePage from "./routes/singlePage/SinglePage";
+//import { listPageLoader } from "../../full-stack-estate/client/src/lib/loaders";
 
-function App(){
-  return (
-    <div className='layout'>
-     <div className='navbar'>
-     <Navbar/>
-     </div>
-    
-    <div className='content'>
-      <HomePage/>
-    </div>
-    </div>
-    
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+         // loder: listPageLoader,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+          // loader: singlePageLoader,
+        },
+      ],
+    },
+  ]);
 
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
